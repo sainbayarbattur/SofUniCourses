ea1192b077ead33991a2f661dd9992a9d83727da
TodorNedkovski <tnedkovski@gmail.com>
2020-03-05 23:39:21 +0200

    Add files via upload

---
+++ Fundamentals Module/Data Types and Variables - Exercise/3.Elevator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp58
+{
+    public class Program
+    {
+        static public void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            int courses = (int)Math.Ceiling((double)n / p);
+            Console.WriteLine(courses);
+        }
+    }
+}
