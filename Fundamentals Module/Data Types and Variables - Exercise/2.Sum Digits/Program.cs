7996166625b843aa11ecfaf96490c3bcc79332c6
TodorNedkovski <tnedkovski@gmail.com>
2020-03-05 23:35:16 +0200

    Add files via upload

---
+++ Fundamentals Module/Data Types and Variables - Exercise/2.Sum Digits/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp58
+{
+    public class Program
+    {
+        static public void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int digit = 0;
+
+            while (n > 0)
+            {
+                digit += n % 10;
+                n /= 10;
+            }
+
+            Console.WriteLine(digit);
+            Console.Read();
+        }
+    }
+}
