9f18d1e210dae8ebfd90fdf625463f5fa5430dd4
TodorNedkovski <tnedkovski@gmail.com>
2020-03-06 15:29:05 +0200

    Add files via upload

---
+++ Fundamentals Module/Data Types and Variables - Exercise/4.Sum of Chars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp58
+{
+    public class Program
+    {
+        static public void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int totalSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                char[] line = Console.ReadLine().ToCharArray();
+                totalSum += line[0];
+            }
+
+            Console.WriteLine($"The sum equals: {totalSum}");
+        }
+    }
+}
