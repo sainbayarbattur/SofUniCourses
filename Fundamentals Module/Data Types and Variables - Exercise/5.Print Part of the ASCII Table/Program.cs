495b5fa5f9833e59d81177750f6b210ed1681e44
TodorNedkovski <tnedkovski@gmail.com>
2020-03-06 15:40:08 +0200

    Add files via upload

---
+++ Fundamentals Module/Data Types and Variables - Exercise/5.Print Part of the ASCII Table/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            string text = "";
+            for (int i = first; i <= second; i++)
+            {
+                text += (char)i + " ";
+            }
+
+            Console.WriteLine(text.TrimEnd());
+        }
+    }
+}
