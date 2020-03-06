1813cc9f7a80837b5637712fe74490ea5952a0a9
TodorNedkovski <tnedkovski@gmail.com>
2020-03-06 15:44:46 +0200

    Add files via upload

---
+++ Fundamentals Module/Data Types and Variables - Exercise/6.Triples of Latin Letters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int b = 0; b < n; b++)
+                {
+                    for (int c = 0; c < n; c++)
+                    {
+                        char first = (char)('a' + i);
+                        char second = (char)('a' + b);
+                        char third = (char)('a' + c);
+                        Console.WriteLine(first.ToString() + second + third);
+                    }
+                }
+            }
+        }
+    }
+}
