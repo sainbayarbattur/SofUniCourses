8ad38b026ffdc33ac017ea025f33e70b933db0a9
TodorNedkovski <tnedkovski@gmail.com>
2020-03-09 00:09:25 +0200

    Add files via upload

---
+++ C# Development/C# Advanced/C# Advanced/Basic Algorithms/2.Recursive Factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02._Recursive_Factorial
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Factorial(int.Parse(Console.ReadLine()), 1));
+        }
+
+        public static long Factorial(int n, int sum)
+        {
+            if (n != 0)
+            {
+                sum *= n;
+                return Factorial(--n, sum);
+            }
+            return sum;
+        }
+    }
+}
