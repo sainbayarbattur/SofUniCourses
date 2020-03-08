3a62ef38632646bf7f48854381b7347a9fad7b7e
TodorNedkovski <tnedkovski@gmail.com>
2020-03-09 00:07:37 +0200

    Add files via upload

---
+++ C# Development/C# Advanced/C# Advanced/Basic Algorithms/1.Recursive Array Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Recursive_Array_Sum
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Sum(Console.ReadLine().Split(" ").Select(int.Parse).ToArray(), 0, 0));
+        }
+
+        private static int Sum(int[] array, int index, int sum)
+        {
+            if (index < array.Length)
+            {
+                sum += array[index];
+                return Sum(array, ++index, sum);
+            }
+
+            return sum;
+        }
+    }
+}
