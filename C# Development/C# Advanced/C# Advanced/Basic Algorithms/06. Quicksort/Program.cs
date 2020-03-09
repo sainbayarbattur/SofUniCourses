432b79421c4b66718dbdc073b74b52bf1313362a
TodorNedkovski <tnedkovski@gmail.com>
2020-03-09 20:34:20 +0200

    Add files via upload

---
+++ C# Development/C# Advanced/C# Advanced/Basic Algorithms/06. Quicksort/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quick_Sort
+{
+    class Program
+    {
+        class Quick
+        {
+            public static void Quick_Sort(int[] arr, int left, int right)
+            {
+                if (left < right)
+                {
+                    int pivot = Partition(arr, left, right);
+
+                    if (pivot > 1)
+                    {
+                        Quick_Sort(arr, left, pivot - 1);
+                    }
+                    if (pivot + 1 < right)
+                    {
+                        Quick_Sort(arr, pivot + 1, right);
+                    }
+                }
+
+            }
+
+            private static int Partition(int[] arr, int left, int right)
+            {
+                int pivot = arr[left];
+                while (true)
+                {
+
+                    while (arr[left] < pivot)
+                    {
+                        left++;
+                    }
+
+                    while (arr[right] > pivot)
+                    {
+                        right--;
+                    }
+
+                    if (left < right)
+                    {
+                        if (arr[left] == arr[right]) return right;
+
+                        int temp = arr[left];
+                        arr[left] = arr[right];
+                        arr[right] = temp;
+
+
+                    }
+                    else
+                    {
+                        return right;
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            Quick.Quick_Sort(arr, 0, arr.Length - 1);
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
