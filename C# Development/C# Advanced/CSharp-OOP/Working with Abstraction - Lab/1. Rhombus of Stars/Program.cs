2400e7a9c36055fb69b3c34ec1ddc5cc686c0d5f
TodorNedkovski <tnedkovski@gmail.com>
2020-04-08 15:30:26 +0300

    Add files via upload

---
+++ C# Development/C# Advanced/CSharp-OOP/Working with Abstraction - Lab/1. Rhombus of Stars/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _1RhombusofStars
+{
+    using System;
+
+    public class Program
+    {
+        const char symbol = '*';
+
+        private static int count;
+
+        public static void Main()
+        {
+            count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= count; i++)
+            {
+                PrintRow(i);
+            }
+
+            for (int i = count - 1; i >= 0; i--)
+            {
+                PrintRow(i);
+            }
+        }
+
+        public static void PrintRow(int n)
+        {
+            string line = new string(' ', count - n);
+
+            for (int i = 0; i < n; i++)
+            {
+                line += symbol + " ";
+            }
+
+            Console.WriteLine(line);
+        }
+    }
+}
