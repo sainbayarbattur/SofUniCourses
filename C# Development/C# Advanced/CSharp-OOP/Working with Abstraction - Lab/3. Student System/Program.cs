389ac6e17e90a080acf4d989c52fc7f9072fae38
TodorNedkovski <tnedkovski@gmail.com>
2020-04-09 11:33:47 +0300

    Add files via upload

---
+++ C# Development/C# Advanced/CSharp-OOP/Working with Abstraction - Lab/3. Student System/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _3StudentSystem
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string line = Console.ReadLine();
+
+            var students = new List<Student>();
+
+            while (line != "Exit")
+            {
+                string command = line.Split(' ')[0];
+
+                string studentName = line.Split(' ')[1];
+
+                if (command == "Show")
+                {
+                    Console.WriteLine(students.Find(s => s.Name == studentName));
+                }
+                else if (command == "Create" && students.Find(s => s.Name == studentName) == null){
+                    int studentAge = int.Parse(line.Split(' ')[2]);
+
+                    double studentGrade = double.Parse(line.Split(' ')[3]);
+
+                    var student = new Student(studentName, studentAge, studentGrade);
+
+                    students.Add(student);
+                }
+
+                line = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ C# Development/C# Advanced/CSharp-OOP/Working with Abstraction - Lab/3. Student System/Student.cs
@@ -0,0 +1,38 @@
+﻿namespace _3StudentSystem
+{
+    public class Student
+    {
+        private double grade;
+
+        public Student(string name, int age, double grade)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.grade = grade;
+        }
+
+        public string Comment 
+        {
+            get
+            {
+                if (this.grade >= 5.50)
+                {
+                    return "Excellent student";
+                }
+                else
+                {
+                    return "Average student";
+                }
+            }
+        }
+
+        public string Name { get; }
+
+        public int Age { get; }
+
+        public override string ToString()
+        {
+            return $"{this.Name} is {this.Age} years old. {this.Comment}.";
+        }
+    }
+}
