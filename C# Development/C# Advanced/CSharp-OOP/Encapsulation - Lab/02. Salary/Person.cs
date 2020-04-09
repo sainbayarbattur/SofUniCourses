3197847a004e868f1d5d6ebabd0903c51a5bf7bc
TodorNedkovski <tnedkovski@gmail.com>
2020-04-10 01:16:18 +0300

    Add files via upload

---
+++ C# Development/C# Advanced/CSharp-OOP/Encapsulation - Lab/02. Salary/Person.cs
@@ -0,0 +1,39 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Salary = salary;
+            this.Age = age;
+        }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public int Age { get; private set; }
+
+        public decimal Salary { get; private set; }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age > 30)
+            {
+                this.Salary += this.Salary * percentage / 100;
+            }
+            else
+            {
+                this.Salary += this.Salary * percentage / 200;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary.ToString("f2")} leva.";
+        }
+    }
+}
---
+++ C# Development/C# Advanced/CSharp-OOP/Encapsulation - Lab/03. Validation/Person.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private decimal salary;
+        private int age;
+        private string firstName;
+        private string lastName;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Salary = salary;
+            this.Age = age;
+        }
+
+        public string FirstName 
+        {
+            get
+            {
+                return this.firstName;
+            }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+
+                this.firstName = value;
+            } 
+        }
+
+        public string LastName 
+        {
+            get 
+            {
+                return this.lastName;
+            } 
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+
+                this.lastName = value;
+            }
+        }
+
+        public int Age 
+        {
+            get
+            {
+                return this.age;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+            private set
+            {
+                if (value < 460)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+                }
+
+                this.salary = value;
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age > 30)
+            {
+                this.Salary += this.Salary * percentage / 100;
+            }
+            else
+            {
+                this.Salary += this.Salary * percentage / 200;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary.ToString("f2")} leva.";
+        }
+    }
+}
