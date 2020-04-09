facafc500422c07eeb2326209e3cbac57dc30f35
TodorNedkovski <tnedkovski@gmail.com>
2020-04-09 15:23:08 +0300

    Add files via upload

---
+++ C# Development/C# Advanced/CSharp-OOP/Inheritance - Lab/01. Single Inheritance/Animal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Single_Inheritance
+{
+    public abstract class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+}
---
+++ C# Development/C# Advanced/CSharp-OOP/Inheritance - Lab/01. Single Inheritance/Dog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Single_Inheritance
+{
+    public class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("barking...");
+        }
+    }
+}
