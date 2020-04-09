da758c3f4d46b118216e01650a337190863ba402
TodorNedkovski <tnedkovski@gmail.com>
2020-04-09 15:22:30 +0300

    Add files via upload

---
+++ C# Development/C# Advanced/C# Advanced/Inheritance - Lab/01. Single Inheritance/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Farm
+{
+    using _01._Single_Inheritance;
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Dog dog = new Dog();
+            dog.Bark();
+            dog.Bark();
+        }
+    }
+}
