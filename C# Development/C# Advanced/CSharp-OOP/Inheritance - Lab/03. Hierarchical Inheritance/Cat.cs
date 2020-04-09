9f6d5b0f6b0bfa6cf514dcb6b952e079022af9a1
TodorNedkovski <tnedkovski@gmail.com>
2020-04-09 15:34:25 +0300

    Add files via upload

---
+++ C# Development/C# Advanced/CSharp-OOP/Inheritance - Lab/03. Hierarchical Inheritance/Cat.cs
@@ -0,0 +1,12 @@
+﻿namespace _01._Single_Inheritance
+{
+    using System;
+
+    public class Cat : Animal
+    {
+        public void Meow()
+        {
+            Console.WriteLine("meowing...");
+        }
+    }
+}
---
+++ C# Development/C# Advanced/CSharp-OOP/Inheritance - Lab/03. Hierarchical Inheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Farm
+{
+    using _01._Single_Inheritance;
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+
+            Cat cat = new Cat();
+            cat.Eat();
+            cat.Meow();
+        }
+    }
+}
