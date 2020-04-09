27a5cea7836deff8a41d22dce845ee0a5641ce6a
TodorNedkovski <tnedkovski@gmail.com>
2020-04-09 13:29:53 +0300

    Add files via upload

---
+++ C# Development/C# Advanced/CSharp-OOP/Working with Abstraction - Lab/4. Hotel Reservation/PriceCalculator.cs
@@ -0,0 +1,55 @@
+﻿namespace _4HotelReservation
+{
+    public static class PriceCalculator
+    {
+        private const int pricePerDaySpring = 2;
+
+        private const int pricePerDaySummer = 4;
+
+        private const int pricePerDayAutumn = 1;
+
+        private const int pricePerDayWinter = 3;
+
+        private const int discountVip = 20;
+
+        private const int secondTimeDiscountVip  = 10;
+
+        public static decimal GetTotalPrice(decimal pricePerDay, int days, string season, string typeClient)
+        {
+            int surcharge = 1;
+            int discount = 1;
+            decimal price = days * pricePerDay;
+
+            if (season == "Spring")
+            {
+                surcharge = pricePerDaySpring;
+            }
+            else if (season == "Summer")
+            {
+                surcharge = pricePerDaySummer;
+            }
+            else if (season == "Autumn")
+            {
+                surcharge = pricePerDayAutumn;
+            }
+            else if (season == "Winter")
+            {
+                surcharge = pricePerDayWinter;
+            }
+
+            if (typeClient == "VIP")
+            {
+                discount = discountVip;
+            }
+            else if (typeClient == "SecondVisit")
+            {
+                discount = secondTimeDiscountVip;
+            }
+
+            price *= surcharge;
+            if (discount != 1) price -= price * (discount / 100m);
+
+            return price;
+        }
+    }
+}
---
+++ C# Development/C# Advanced/CSharp-OOP/Working with Abstraction - Lab/4. Hotel Reservation/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _4HotelReservation
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split(' ');
+
+            decimal pricePerDay = decimal.Parse(input[0]);
+            int numberOfDays = int.Parse(input[1]);
+            string season = input[2];
+
+            string typeClient = string.Empty;
+
+            if (input.Length == 4)
+            {
+                typeClient = input[3];
+            }
+
+            Console.WriteLine(PriceCalculator.GetTotalPrice(pricePerDay, numberOfDays, season, typeClient).ToString("f2"));
+
+        }
+    }
+}
