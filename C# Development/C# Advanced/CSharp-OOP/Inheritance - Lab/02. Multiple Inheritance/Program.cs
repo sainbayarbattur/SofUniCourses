dae608eb47aad881e9a04f9ecc255636896dea8e
TodorNedkovski <tnedkovski@gmail.com>
2020-04-09 15:27:07 +0300

    Add files via upload

---
+++ C# Development/C# Advanced/CSharp-OOP/Inheritance - Lab/02. Multiple Inheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Farm
+{
+    using _01._Single_Inheritance;
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Puppy puppy = new Puppy();
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+
+        }
+    }
+}
---
+++ C# Development/C# Advanced/CSharp-OOP/Inheritance - Lab/02. Multiple Inheritance/Puppy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Single_Inheritance
+{
+    public class Puppy : Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("weeping...");
+        }
+    }
+}
