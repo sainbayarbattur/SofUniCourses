ace2ac77fb94cab180a9974b5548d0b283d9b105
TodorNedkovski <tnedkovski@gmail.com>
2020-04-10 14:34:35 +0300

    Add files via upload

---
+++ C# Development/C# Advanced/CSharp-OOP/Interfaces and Abstraction - Lab/02. Cars/Model/Tesla.cs
@@ -0,0 +1,33 @@
+﻿namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int betteries)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Battery = betteries;
+        }
+
+        public int Battery { get; }
+
+        public string Model { get; }
+
+        public string Color { get; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} Tesla {this.Model} with {this.Battery} Batteries";
+        }
+    }
+}
