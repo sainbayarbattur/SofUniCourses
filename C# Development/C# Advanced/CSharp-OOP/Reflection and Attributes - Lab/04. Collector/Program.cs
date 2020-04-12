7c3b53703c63c31ef0f1c19ef61857cd269489e5
TodorNedkovski <tnedkovski@gmail.com>
2020-04-12 23:49:06 +0300

    Add files via upload

---
+++ C# Development/C# Advanced/CSharp-OOP/Reflection and Attributes - Lab/04. Collector/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _1Stealer
+{
+    using System;
+    using _1Stealer.Model;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Spy spy = new Spy();
+            string result = spy.CollectGettersAndSetters("Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
