c5338e9364c5758f2b78b543bcab3de16a3d439e
TodorNedkovski <tnedkovski@gmail.com>
2020-04-13 00:08:51 +0300

    Add files via upload

---
+++ C# Development/C# Advanced/CSharp-OOP/Reflection and Attributes - Lab/06. Code Tracker/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _5CreateAttribute
+{
+    [Author("Ventsi")]
+    public class StartUp
+    {
+        [Author("Gosho")]
+        public static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
