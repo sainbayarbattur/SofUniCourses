029321a027e47e105094a4e0512e168f41a64c0c
TodorNedkovski <tnedkovski@gmail.com>
2020-04-12 21:25:12 +0300

    Add files via upload

---
+++ C# Development/C# Advanced/CSharp-OOP/Reflection and Attributes - Lab/01. Stealer/Model/Spy.cs
@@ -0,0 +1,30 @@
+﻿namespace _1Stealer.Model
+{
+    using System;
+    using System.Text;
+    using System.Linq;
+    using System.Reflection;
+
+    public class Spy
+    {
+        public string StealFieldInfo(string name, params string[] fieldsToInvestigate)
+        {
+            var sb = new StringBuilder();
+
+            var type = Type.GetType($"_1Stealer.Model.{name}");
+
+            var fields = type.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic);
+
+            var instance = Activator.CreateInstance(type);
+
+            sb.AppendLine($"Class under investigation: {name}");
+
+            foreach (var field in fields.Where(f => fieldsToInvestigate.Contains(f.Name)))
+            {
+                sb.AppendLine(field.Name + " = " + field.GetValue(instance));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
