b9f1055e28d82dc82276db6cca80b4bdbce7bd63
TodorNedkovski <tnedkovski@gmail.com>
2020-04-15 14:34:32 +0300

    Add files via upload

---
+++ C# Development/C# Advanced/CSharp-OOP/Unit Testing - Lab/01. Test Axe/AxeTests.cs
@@ -0,0 +1,47 @@
+﻿namespace Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    public class AxeTests
+    {
+        private Axe axe;
+
+        const int axeAttack = 10;
+        const int axeDurability = 10;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.axe = new Axe(axeAttack, axeDurability);
+        }
+
+        [Test]
+        public void TestAxeConstructor()
+        {
+            Assert.AreEqual(this.axe.AttackPoints, axeAttack);
+            Assert.AreEqual(this.axe.DurabilityPoints, axeDurability);
+        }
+
+        [Test]
+        [TestCase(10, 10)]
+        public void TestAttackMethod(int health, int experience)
+        {
+            var dummy = new Dummy(health, experience);
+
+            axe.Attack(dummy);
+
+            Assert.AreEqual(axe.DurabilityPoints, 9);
+        }
+
+        [Test]
+        [TestCase(10, 0)]
+        [TestCase(20, -1)]
+        public void TestAttackMethodShouldThrowExeption(int attackPoints, int durabilityPoints)
+        {
+            var axe = new Axe(attackPoints, durabilityPoints);
+
+            Assert.Throws<InvalidOperationException>(() => axe.Attack(new Dummy(1, 1)));
+        }
+    }
+}
---
+++ C# Development/C# Advanced/CSharp-OOP/Unit Testing - Lab/02. Dummy Tests/DummyTests.cs
@@ -0,0 +1,57 @@
+﻿namespace Tests
+{
+    using NUnit.Framework;
+    using System;
+
+    public class DummyTests
+    {
+        private Dummy dummy;
+
+        private const int health = 10;
+        private const int experience = 10;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.dummy = new Dummy(health, experience);
+        }
+
+        [Test]
+        public void TestIsDeadMethod()
+        {
+            var dummy = new Dummy(0, 0);
+
+            Assert.IsTrue(dummy.Health <= 0);
+        }
+
+        [Test]
+        public void TestTakeAttackMethod()
+        {
+            dummy.TakeAttack(10);
+
+            Assert.AreEqual(dummy.Health, 0);
+        }
+
+        [Test]
+        public void TestTakeAttackMethodShouldThrowExeption()
+        {
+            var dummy = new Dummy(0, 0);
+
+            Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(12));
+        }
+
+        [Test]
+        public void TestGiveExperienceMethod()
+        {
+            dummy.TakeAttack(10);
+
+            Assert.AreEqual(dummy.GiveExperience(), 10);
+        }
+
+        [Test]
+        public void TestGiveExperienceMethodShouldThrowExeption()
+        {
+            Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
+        }
+    }
+}
