7f40dd9a804383f2f99745e680e3dbe22e1e61bf
TodorNedkovski <tnedkovski@gmail.com>
2020-04-22 14:51:14 +0300

    Add files via upload

---
+++ C# Development/C# Advanced/C# Advanced/Defining Classes - Lab/5. Special Cars/Car.cs
@@ -0,0 +1,96 @@
+﻿namespace CarManufacturer
+{
+    using System;
+    using System.Text;
+
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+        private Engine engine;
+        private Tire[] tires;
+
+        public Car()
+        {
+            this.Make = "VW";
+            this.Model = "Golf";
+            this.Year = 2025;
+            this.FuelQuantity = 200;
+            this.FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year)
+            : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsuption)
+            : this(make, model, year)
+        {
+            this.FuelConsumption = fuelConsuption;
+            this.FuelQuantity = fuelQuantity;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsuption, Engine engine, Tire[] tires)
+            : this(make, model, year, fuelQuantity, fuelConsuption)
+        {
+            this.engine = engine;
+            this.tires = tires;
+        }
+
+        public void Drive(double distance)
+        {
+            if (this.FuelQuantity - ((distance / 100) * this.FuelConsumption) > 0)
+            {
+                this.FuelQuantity -= ((distance / 100) * this.FuelConsumption);
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            var result = new StringBuilder();
+
+            result.AppendLine($"Make: {this.Make}");
+            result.AppendLine($"Model: {this.Model}");
+            result.AppendLine($"Year: {this.Year}");
+            result.Append($"Fuel: {this.FuelQuantity:F2}L");
+
+            return result.ToString();
+        }
+
+        public override string ToString()
+        {
+            string result = $"Make: {this.Make}\n" +
+            $"Model: {this.Model}\n" +
+            $"Year: {this.Year}\n" +
+            $"HorsePowers: {this.Engine.HorsePower}\n" +
+            $"FuelQuantity: {this.FuelQuantity}";
+
+            return result;
+        }
+
+        public Engine Engine { get => this.engine; set { this.engine = value; } }
+
+        public Tire[] Tires { get => this.tires; set { this.tires = value; } }
+
+        public string Make { get => this.make; set { this.make = value; } }
+
+        public string Model { get => this.model; set { this.model = value; } }
+
+        public int Year { get => this.year; set { this.year = value; } }
+
+        public double FuelQuantity { get => this.fuelQuantity; set { this.fuelQuantity = value; } }
+
+        public double FuelConsumption { get => this.fuelConsumption; set { this.fuelConsumption = value; } }
+    }
+}
---
+++ C# Development/C# Advanced/C# Advanced/Defining Classes - Lab/5. Special Cars/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace CarManufacturer
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string command = Console.ReadLine();
+
+            var tiresList = new List<Tire[]>();
+            var enginesList = new List<Engine>();
+            var carsList = new List<Car>();
+
+            while (command != "No more tires")
+            {
+                string[] tires = command.Split(' ');
+
+                var tirePair = new List<Tire>();
+                int count = 1;
+
+                while (tirePair.Count != 4)
+                {
+                    int year = int.Parse(tires[count - 1]);
+                    double pressure = double.Parse(tires[count]);
+
+                    tirePair.Add(new Tire(year, pressure));
+
+                    count += 2;
+                }
+
+                tiresList.Add(tirePair.ToArray());
+
+                command = Console.ReadLine();
+            }
+
+            command = Console.ReadLine();
+
+            while (command != "Engines done")
+            {
+                int horsePower = int.Parse(command.Split(' ')[0]);
+                double cubicCapacity = double.Parse(command.Split(' ')[1]);
+
+                enginesList.Add(new Engine(horsePower, cubicCapacity));
+
+                command = Console.ReadLine();
+            }
+
+            command = Console.ReadLine();
+
+            while (command != "Show special")
+            {
+                string make = command.Split(' ')[0];
+                string model = command.Split(' ')[1];
+                int year = int.Parse(command.Split(' ')[2]);
+                double fuelQuantity = double.Parse(command.Split(' ')[3]);
+                double fuelConsumption = int.Parse(command.Split(' ')[4]);
+                int engineIndex = int.Parse(command.Split(' ')[5]);
+                int tiresIndex = int.Parse(command.Split(' ')[6]);
+                Engine engine = enginesList[engineIndex];
+                Tire[] tires = tiresList[tiresIndex];
+
+                carsList.Add(new Car(make, model, year, fuelQuantity, fuelConsumption, engine, tires));
+
+                command = Console.ReadLine();
+            }
+
+            var specialCars = carsList
+                .Where(c => c.Year >= 2017
+                && c.Engine.HorsePower > 330
+                && c.Tires.Sum(t => t.Pressure) >= 9
+                && c.Tires.Sum(t => t.Pressure) <= 10)
+                .ToList();
+
+            foreach (var car in specialCars)
+            {
+                car.Drive(20);
+
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
