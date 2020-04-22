6a496a9ccd314d08dd5bb2b2111975fb7908d0e8
TodorNedkovski <tnedkovski@gmail.com>
2020-04-22 20:36:11 +0300

    Add files via upload

---
+++ C# Development/C# Advanced/C# Advanced/Generics - Exercise/09. Custom Linked List/CustomLinkedList.cs
@@ -0,0 +1,45 @@
+﻿namespace Generics___Exercise
+{
+    using System.Linq;
+
+    public class CustomLinkedList<T>
+    {
+        private T[] collection;
+        private readonly T T_DEFAULT = default(T);
+
+        public CustomLinkedList(int length)
+        {
+            this.collection = new T[length];
+        }
+
+        public int Count => this.collection.Where(c => c != null).Count();
+
+        public CustomLinkedList<T> AddLast(T item) 
+        {
+            this.collection[this.collection.Length - 1] = item;
+
+            return this;
+        }
+
+        public CustomLinkedList<T> AddFirst(T item)
+        {
+            this.collection[0] = item;
+
+            return this;
+        }
+
+        public CustomLinkedList<T> RemoveLast(T item)
+        {
+            this.collection[this.collection.Length - 1] = T_DEFAULT;
+
+            return this;
+        }
+
+        public CustomLinkedList<T> RemoveFirst(T item)
+        {
+            this.collection[0] = T_DEFAULT;
+
+            return this;
+        }
+    }
+}
---
+++ C# Development/C# Advanced/C# Advanced/Generics - Exercise/09. Custom Linked List/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Generics___Exercise
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var l = new LinkedList<string>();
+        }
+    }
+}
