9b6e18fa05075b08718197f2f22055ec55d80e00
TodorNedkovski <tnedkovski@gmail.com>
2020-04-22 18:08:08 +0300

    Add files via upload

---
+++ C# Development/C# Advanced/C# Advanced/Defining Classes - Exercise/10. SoftUni Parking/Parking.cs
@@ -0,0 +1,78 @@
+﻿namespace SoftUniParking
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class Parking
+    {
+        public Car[] cars;
+        private int capacity;
+
+        public Parking(int capacity)
+        {
+            this.cars = new Car[capacity];
+            this.capacity = capacity;
+        }
+
+        public int Count => cars.Where(c => c != null).Count();
+
+        public string AddCar(Car car)
+        {
+            if (this.cars.Any(c => c != null && c.RegistrationNumber == car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+
+            if (this.Count + 1 > this.capacity)
+            {
+                return "Parking is full!";
+            }
+
+            this.cars[this.Count] = car;
+
+            return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            Car carToRemove = this.cars.FirstOrDefault(c => c != null && c.RegistrationNumber == registrationNumber);
+
+            if (!this.cars.Any(c => c != null && c.RegistrationNumber == registrationNumber) || carToRemove == null)
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+
+            var index = Array.IndexOf(this.cars, carToRemove);
+
+            this.cars[index] = null;
+
+            return $"Successfully removed {registrationNumber}";
+        }
+
+        public Car GetCar(string registrationNumber)
+        {
+            return this.cars.FirstOrDefault(c => c.RegistrationNumber == registrationNumber);
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            for (int i = 0; i < registrationNumbers.Count; i++)
+            {
+                string currentRegistrationNumber = registrationNumbers[i];
+
+                if (this.cars.Any(c => c != null && c.RegistrationNumber == currentRegistrationNumber))
+                {
+                    Car carToRemove = this.cars[i];
+
+                    if (carToRemove != null)
+                    {
+                        var index = Array.IndexOf(this.cars, carToRemove);
+
+                        this.cars[index] = null;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C# Development/C# Advanced/C# Advanced/Defining Classes - Exercise/10. SoftUni Parking/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftUniParking
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var car = new Car("Skoda", "Fabia", 65, "CC1856BG");
+            var car2 = new Car("Audi", "A3", 110, "EB8787MN");
+            var car3 = new Car("Audi1234", "A3", 110, "EB878*MN");
+
+            var parking = new Parking(3);
+
+            Console.WriteLine(parking.AddCar(car));
+            Console.WriteLine(parking.AddCar(car2));
+            Console.WriteLine(parking.AddCar(car3));
+
+            parking.RemoveSetOfRegistrationNumber(new List<string> { "CC1856BG", "EB8787MN" });
+
+            Console.WriteLine(parking.cars[2]);
+        }
+    }
+}
