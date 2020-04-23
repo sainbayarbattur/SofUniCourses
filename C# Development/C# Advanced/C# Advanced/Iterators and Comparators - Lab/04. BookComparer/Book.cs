070f4f1423518f84e288c40163d9d6810cda7b1b
TodorNedkovski <tnedkovski@gmail.com>
2020-04-24 00:09:02 +0300

    Add files via upload

---
+++ C# Development/C# Advanced/C# Advanced/Iterators and Comparators - Lab/04. BookComparer/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = authors;
+        }
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public IReadOnlyList<string> Authors { get; set; }
+
+        public int CompareTo(Book other)
+        {
+            if (this.Year == other.Year)
+            {
+                return this.Title.CompareTo(other.Title);
+            }
+
+            return this.Year.CompareTo(other.Year);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year}";
+        }
+    }
+}
---
+++ C# Development/C# Advanced/C# Advanced/Iterators and Comparators - Lab/04. BookComparer/BookComparator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace IteratorsAndComparators
+{
+    public class BookComparator : IComparer<Book>
+    {
+        public int Compare(Book firstBook, Book secondBook)
+        {
+
+            if (firstBook.Title.CompareTo(secondBook.Title) == 0)
+            {
+                return secondBook.Year.CompareTo(firstBook.Year);
+            }
+
+            return firstBook.Title.CompareTo(secondBook.Title);
+        }
+    }
+}
