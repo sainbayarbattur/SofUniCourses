b1445e7f156612cc4af97e320d44877b7d3a2a86
TodorNedkovski <tnedkovski@gmail.com>
2020-04-24 10:50:15 +0300

    Add files via upload

---
+++ C# Development/C# Advanced/C# Advanced/Iterators and Comparators - Exercise/07. Custom Linked List/CustomLinkedList.cs
@@ -0,0 +1,57 @@
+﻿namespace Generics___Exercise
+{
+    using System;
+    using System.Linq;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class CustomLinkedList<T> : IEnumerable<T>
+    {
+        private List<T> collection;
+
+        public CustomLinkedList()
+        {
+            this.collection = new List<T>();
+        }
+
+        public int Count => this.collection.Where(c => c != null).Count();
+
+        public CustomLinkedList<T> AddLast(T item) 
+        {
+            this.collection.Insert(this.collection.Count - 1, item);
+
+            return this;
+        }
+
+        public CustomLinkedList<T> AddFirst(T item)
+        {
+            this.collection.Insert(0, item);
+
+            return this;
+        }
+
+        public CustomLinkedList<T> RemoveLast()
+        {
+            this.collection.RemoveAt(this.collection.Count - 1);
+
+            return this;
+        }
+
+        public CustomLinkedList<T> RemoveFirst()
+        {
+            this.collection.RemoveAt(0);
+
+            return this;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.collection.Count; i++)
+            {
+                yield return this.collection[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
---
+++ C# Development/C# Advanced/C# Advanced/Iterators and Comparators - Exercise/07. Custom Linked List/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _07._Custom_Linked_List
+{
+    using Generics___Exercise;
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var cll = new CustomLinkedList<int>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                cll.AddFirst(i);
+            }
+
+            foreach (var item in cll)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
