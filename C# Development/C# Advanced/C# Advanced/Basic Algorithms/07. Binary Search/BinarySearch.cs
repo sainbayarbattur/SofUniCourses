8d5ea55417d6be6a5c86461b124fbf5b4aabb2ed
TodorNedkovski <tnedkovski@gmail.com>
2020-04-24 18:01:11 +0300

    Add files via upload

---
+++ C# Development/C# Advanced/C# Advanced/Basic Algorithms/07. Binary Search/BinarySearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._Binary_Search
+{
+    public static class BinarySearch
+    {
+        public static int Search<T>(T[] arr, T item, int left, int right)
+        {
+            if (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (Comparer<T>.Default.Compare(item, arr[mid]) == 0)
+                {
+                    return mid;
+                }
+
+                if (Comparer<T>.Default.Compare(item, arr[mid]) > 0)
+                {
+                    return Search(arr, item, mid + 1, right);
+                }
+
+                return Search(arr, item, left, mid - 1);
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ C# Development/C# Advanced/C# Advanced/Basic Algorithms/07. Binary Search/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _07._Binary_Search
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int item = int.Parse(Console.ReadLine());
+
+            var index = BinarySearch.Search(arr, item, 0, arr.Length - 1);
+            Console.WriteLine(index);
+        }
+    }
+}
