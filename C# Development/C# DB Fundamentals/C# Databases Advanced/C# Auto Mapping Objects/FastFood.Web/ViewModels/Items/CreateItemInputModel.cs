6cf8c28f4e1c8cdb3739f3bad4f17dcd838d0108
TodorNedkovski <tnedkovski@gmail.com>
2020-04-27 15:45:56 +0300

    pushing automaping c# exercieses

---
+++ C# Development/C# DB Fundamentals/C# Databases Advanced/C# Auto Mapping Objects/FastFood.Web/ViewModels/Items/CreateItemInputModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FastFood.Web.ViewModels.Items
+{
+    public class CreateItemInputModel
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
