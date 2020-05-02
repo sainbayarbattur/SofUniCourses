77f6167b43b5625da280b5b5cfa7fd08412f6115
TodorNedkovski <tnedkovski@gmail.com>
2020-05-02 19:30:21 +0300

    fixinh bugs in code first approach

--- C# Development/C# DB Fundamentals/C# Databases Advanced/Code-First/1. Hospital Database/Program.cs
+++ C# Development/C# DB Fundamentals/C# Databases Advanced/Code-First/1. Hospital Database/Program.cs
@@ -1,40 +1,70 @@
 ﻿namespace P01_HospitalDatabase
 {
-    using P01_HospitalDatabase.Core;
+    using Microsoft.EntityFrameworkCore;
     using P01_HospitalDatabase.Data;
+    using P01_HospitalDatabase.Data.Models;
     using System;
+    using System.Linq;
 
-    public class StartUp
+    public class Program
     {
         public static void Main()
         {
-            using (var context = new HospitalContext())
-            {
-                try
-                {
-                    Console.WriteLine("Welcome guest! Do you have an account?");
-                    Console.Write("Please write Y/N: ");
-                    string answer = Console.ReadLine();
-                    var commandUI = new CommandUserInterface();
+            using var db = new HospitalContext();
 
-                    if (answer.ToUpper() == "Y")
-                    {
-                        commandUI.Login(context);
-                    }
-                    else if (answer.ToUpper() == "N")
-                    {
-                        commandUI.Register(context);
-                    }
-                    else
-                    {
-                        throw new ArgumentException("Incorrect answer!");
-                    }
-                }
-                catch (ArgumentException e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-            }
+            //var patient = new Patient
+            //{
+            //    FirstName = "Valeri",
+            //    LastName = "Stopkanov",
+            //    Address = "Lulin",
+            //    Email = "babichikiobicamdaqm@abv.bg",
+            //    HasInsurance = false,
+            //};
+
+            //var medicament = new Medicament
+            //{
+            //    Name = "Heroin"
+            //};
+
+            //db.Medicaments.Find(1).Prescriptions
+            //    .Add(new PatientMedicament
+            //    {
+            //        MedicamentId = 1,
+            //        PatientId = 1
+            //    });
+
+            var medicament = db.Medicaments.Find(1);
+            var patient = db.Patients.Find(2);
+
+            db.PatientsMedicaments.Add(new PatientMedicament
+            {
+                Medicament = medicament,
+                Patient = patient
+            });
+
+            //db.Database.Migrate();
+
+            //var p = db.PatientsMedicaments.First(pm => pm.PatientId == 1);
+
+            //Console.WriteLine(p.Patient.FirstName);
+
+            //medicament.Prescriptions.Add(new PatientMedicament 
+            //{
+            //    Patient = patient,
+            //    Medicament = medicament
+            //});
+
+            //db.Medicaments.Add(medicament);
+
+            //patient.Prescriptions.Add(new PatientMedicament
+            //{
+            //    Medicament = medicament,
+            //    Patient = patient
+            //});
+
+            //db.Patients.Add(patient);
+
+            db.SaveChanges();
         }
     }
 }
