3517accb06f427a17658d5d570b64ab5463d822c
TodorNedkovski <tnedkovski@gmail.com>
2020-05-24 01:02:00 +0300

    adding cookies

--- C# Development/C# Web/HttpRequester/Program.cs
+++ C# Development/C# Web/HttpRequester/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace HttpRequester
 {
     using System;
+    using System.Collections.Generic;
     using System.Data;
     using System.IO;
     using System.Linq;
@@ -13,8 +14,12 @@
 
     public class Program
     {
+        static Dictionary<string, int> SessionStore = new Dictionary<string, int>();
+
         public static async Task Main()
         {
+            const string newLine = "\r\n";
+
             TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 80);
             tcpListener.Start();
 
@@ -28,19 +33,40 @@
                 string request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
                 Console.WriteLine(request);
 
-                string responseText = "<img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg\" />>";
+                //string responseText = "<img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg\" />>";
 
-                var catBytes = File.ReadAllBytes("./../../../Cat03.jpg");
-                var responseTextBytes = Encoding.UTF8.GetBytes(responseText);
+                //var catBytes = await File.ReadAllBytesAsync("./../../../Cat03.jpg");
+                //var responseTextBytes = Encoding.UTF8.GetBytes(responseText);
 
-                string response = 
-                "HTTP/1.1 200 OK"
-                    + "\r\n" + "Server: Softuni/1.0"
-                    + "\r\n" + "Content-Type: text/html"
-                    + "\r\n" + $"Content-Length: {responseTextBytes.Count()}"
-                    + "\r\n"
-                    + "\r\n"
-                    + responseText;
+                var sid = Regex.Match(request, @"sid=[^\n]*\n").Value?.Replace("sid=", string.Empty).Trim();
+                Console.WriteLine(sid);
+                var newSid = Guid.NewGuid().ToString();
+                var count = 0;
+                if (SessionStore.ContainsKey(sid))
+                {
+                    SessionStore[sid]++;
+                    count = SessionStore[sid];
+                }
+                else
+                {
+                    sid = null;
+                    SessionStore[newSid] = 1;
+                    count = 1;
+                }
+                string responseText = $"<h1>{count}<h1>";
+                var responceBytes = Encoding.UTF8.GetBytes(responseText);
+
+                string response =
+                    "HTTP/1.1 200 OK"
+                        + newLine + "Server: Softuni/1.0"
+                        + newLine + "Content-Type: text/html"
+                        + newLine + $"Content-Length: {responceBytes.Count()}"
+                        + newLine + (string.IsNullOrWhiteSpace(sid) ?
+                                ("Set-Cookie: sid=" + newSid + newLine)
+                                : string.Empty)
+                        + newLine
+                        + newLine
+                        + responseText;
 
                 var responseBytes = Encoding.UTF8.GetBytes(response);
 
